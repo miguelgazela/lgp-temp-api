d4c107cc6097a6066f0a2a700e799377c6882cc6
miguelgazela <miguel.gazela@gmail.com>
2014-04-13 10:27:32 +0100

    Added new API resource to return a list of products

--- index.php
+++ index.php
@@ -29,14 +29,40 @@
     $app->get('/products/:productId', function ($productId) use ($app) {
         $app->contentType('application/json');
         $result = array(
-                "product" => array(
-                    "id" => $productId,
-                    "name" => "Product #".$productId,
-                    "description" => "This will contain the full description of the product. It will be displayed in the ProductDetail Activity",
-                    "smallDescription" => "Small description displayed in products lists"
-                )
+            "product" => array(
+                "id" => $productId,
+                "name" => "Product #".$productId,
+                "description" => "This will contain the full description of the product. It will be displayed in the ProductDetail Activity",
+                "smallDescription" => "Small description displayed in products lists"
+            )
         );
         echo json_encode($result);
+    });
+
+    $app->get('/products/', function () use ($app) {
+        $app->contentType('application/json');
+        $result = array(
+            "products" => array(
+                array(
+                    "id" => "1",
+                    "name" => "Product #1",
+                    "description" => "This will contain the full description of the product. It will be displayed in the ProductDetail Activity",
+                    "smallDescription" => "Small description displayed in products lists"
+                ),
+                array(
+                    "id" => "2",
+                    "name" => "Product #2",
+                    "description" => "This will contain the full description of the product. It will be displayed in the ProductDetail Activity",
+                    "smallDescription" => "Small description displayed in products lists"
+                ),
+                array(
+                    "id" => "3",
+                    "name" => "Product #3",
+                    "description" => "This will contain the full description of the product. It will be displayed in the ProductDetail Activity",
+                    "smallDescription" => "Small description displayed in products lists"
+                ),
+            )
+        );
     });
 
     $app->run();
