c7f06ffa6c77f259957464f36c4bf03496d2fe00
miguelgazela <miguel.gazela@gmail.com>
2014-05-23 22:19:27 +0100

    Changed the database to the remote

--- database.php
+++ database.php
@@ -6,27 +6,27 @@
  
     $capsule = new Capsule; 
      
-    // $capsule->addConnection(array(
-    //     'driver'    => 'mysql',
-    //     'host'      => 'db.fe.up.pt',
-    //     'database'  => 'ei10076',
-    //     'username'  => 'ei10076',
-    //     'password'  => 'PC14GSA25',
-    //     'charset'   => 'utf8',
-    //     'collation' => 'utf8_unicode_ci',
-    //     'prefix'    => ''
-    // ));
-
     $capsule->addConnection(array(
         'driver'    => 'mysql',
-        'host'      => '127.0.0.1',
-        'database'  => 'bulla_app',
-        'username'  => 'root',
-        'password'  => 'BKsrUgn6',
+        'host'      => 'db.fe.up.pt',
+        'database'  => 'ei10076',
+        'username'  => 'ei10076',
+        'password'  => 'PC14GSA25',
         'charset'   => 'utf8',
         'collation' => 'utf8_unicode_ci',
         'prefix'    => ''
     ));
+
+    // $capsule->addConnection(array(
+    //     'driver'    => 'mysql',
+    //     'host'      => '127.0.0.1',
+    //     'database'  => 'bulla_app',
+    //     'username'  => 'root',
+    //     'password'  => 'BKsrUgn6',
+    //     'charset'   => 'utf8',
+    //     'collation' => 'utf8_unicode_ci',
+    //     'prefix'    => ''
+    // ));
      
     $capsule->bootEloquent();
 
